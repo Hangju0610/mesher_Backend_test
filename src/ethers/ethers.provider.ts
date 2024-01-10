e2a5c5ab40870da791caa2424e84e3c4eb966e1b
hangju0610 <phj950610@naver.com>
2024-01-10 18:23:32 +0900

    Feat: Add Block and Transaction description Block 정보와 Transaction 정보를 가져오는 description 생성

--- src/ethers/ethers.provider.ts
+++ src/ethers/ethers.provider.ts
@@ -1,16 +1,16 @@
-import { OnModuleInit } from '@nestjs/common';
+import { OnApplicationBootstrap, OnModuleInit } from '@nestjs/common';
 import { ethers } from 'ethers';
 /**
  * 블록체인 데이터 조회를 위한 ethers 연결 Providers
  */
-export class ethersProvider implements OnModuleInit {
-  onModuleInit() {
+export class ethersProvider implements OnApplicationBootstrap {
+  onApplicationBootstrap() {
     const provider = new ethers.providers.InfuraProvider();
     console.log(
       provider.connection.allowGzip == true ? '연결 완료' : '연결 에러!',
     );
-    provider.on('block', (block) => {
-      console.log(block);
+    provider.on('block', async (block) => {
+      console.log(await provider.getBlockWithTransactions(block));
     });
   }
 }
