f61e4af2e0bf977ea6c1716b040f314b11c5b9ad
hangju0610 <phj950610@naver.com>
2024-01-11 13:56:03 +0900

    Feat: Add Block data 메세지 전달 시, 자동으로 Data를 넣는 메서드 완성

--- src/ethers/ethers.module.ts
+++ src/ethers/ethers.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EthersProcessor } from './ethers.processor';
 import { ethersProvider } from './ethers.provider';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Block } from 'src/block/entities/block.entity';
+import { TxReceipt } from 'src/txreceipt/entities/txreceipt.entity';
+import { BlockLog } from 'src/blocklog/entities/blocklog.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Block, TxReceipt, BlockLog])],
   providers: [ethersProvider, EthersProcessor],
 })
 export class EthersModule {}
