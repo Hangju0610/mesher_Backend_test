c007d9242e406dd4621c4675002e8d426b123376
hangju0610 <phj950610@naver.com>
2024-01-11 17:35:29 +0900

    Feat: Add FindReceiptByHash method Hash를 통해 Receipt를 찾는 메서드

--- src/txreceipt/txreceipt.controller.ts
+++ src/txreceipt/txreceipt.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { TxReceipt } from './entities/txreceipt.entity';
+import { TxReceiptService } from './txreceipt.service';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('txreceipts')
-export class TxReceiptController {}
+export class TxReceiptController {
+  constructor(private readonly txReceiptService: TxReceiptService) {}
+
+  @Get()
+  @ApiOkResponse({ type: TxReceipt, description: 'Hash를 통해 찾습니다.' })
+  async findReceiptByHash(
+    @Query('transaction') transactionHash: string,
+  ): Promise<TxReceipt> {
+    return this.txReceiptService.findReceiptByHash(transactionHash);
+  }
+}
--- src/txreceipt/txreceipt.service.ts
+++ src/txreceipt/txreceipt.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TxReceipt } from './entities/txreceipt.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TxReceiptService {}
+export class TxReceiptService {
+  constructor(
+    @InjectRepository(TxReceipt)
+    private readonly txReceiptRepository: Repository<TxReceipt>,
+  ) {}
+
+  /**
+   * Hash를 통해 transactionReceipt를 찾는 Method
+   * @param hash transactionHash
+   * @returns
+   */
+  async findReceiptByHash(transactionHash: string): Promise<TxReceipt> {
+    return await this.txReceiptRepository.findOneBy({ transactionHash });
+  }
+}
