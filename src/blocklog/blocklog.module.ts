1a65de98a4d7c3bfa8874f3f761471307b79fba5
hangju0610 <phj950610@naver.com>
2024-01-11 23:37:27 +0900

    Feat: add countBlockLogs method

--- src/blocklog/blocklog.module.ts
+++ src/blocklog/blocklog.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlockLogService } from './blocklog.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlockLog } from './entities/blocklog.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BlockLog])],
   providers: [BlockLogService],
 })
 export class BlockLogModule {}
